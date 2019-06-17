e4bcd4ffe7192ced14088b01db7960ef3993452d
harry <h.masson1911@gmail.com>
2019-06-17 11:36:15 -0400

    Hello Git!

--- src/coolc.sh
+++ src/coolc.sh
@@ -1,12 +1,14 @@
+
 # Incluya aquí las instrucciones necesarias para ejecutar su compilador
 
-INPUT_FILE=$1
-OUTPUT_FILE=${INPUT_FILE:0: -2}mips
+# INPUT_FILE=$1
+# OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
 # Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
-echo "LINEA_CON_NOMBRE_Y_VERSION_DEL_COMPILADOR"   # Recuerde cambiar estas
-echo "Copyright (c) 2019: Nombre1, Nombre2, Nombre3"    # líneas a los valores correctos
+echo "COOL Compiler Version 1.0 of June 13, 2019."   # Recuerde cambiar estas
+echo "Copyright (c) 2019 School of Math and Computer Science, University of Havana: Hector, Frankie and Frank."    # líneas a los valores correctos
 
 # Llamar al compilador
 # echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
 
+python3 coolc.py $@
